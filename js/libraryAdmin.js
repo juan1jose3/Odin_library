57c31c5d77b58053a8dd567317d6f8e2b80aa194
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-07-27 15:58:41 -0500

    added objects to list

--- js/libraryAdmin.js
+++ js/libraryAdmin.js
@@ -1,9 +1,47 @@
 const myLibrary = [];
 
-function Book() {
+function Book(id,title, author, pages) {
   // the constructor...
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
-function addBookToLibrary() {
+Book.prototype.bookInfo = function () {
+  return `${this.title} ${this.author} ${this.pages}`;
+};
+
+
+function addBookToLibrary(title, author, pages) {
   // take params, create a book then store it in the array
+  const id = crypto.randomUUID();
+  //console.log(id);
+  let bookItem = new Book(id, title, author, pages);
+  //console.log(bookItem.bookInfo());
+
+  myLibrary.push(bookItem);
+  console.log(myLibrary);
+  
 }
+
+function fetchFormData() {
+  const modalForm = document.querySelector("form");
+
+  modalForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const bookTitle = data.get("bookTitle");
+    const bookAuthor = data.get("bookAuthor");
+    const bookPages = data.get("bookPages");
+
+    addBookToLibrary(bookTitle, bookAuthor, bookPages);
+    
+  });
+}
+
+fetchFormData();
+
+
+
+
